c38740a899f3ff4737bdba5198fd560ad96c5672
Apurv Kumaria <kumaria.apurv@gmail.com>
2013-05-08 01:23:34 -0500

    Added code for loading and reading from cassandra

--- edu/harvard/e185/word_count_reader.py
+++ edu/harvard/e185/word_count_reader.py
@@ -1,2 +1,52 @@
+import pycassa
+from pycassa.pool import ConnectionPool
+from pycassa.system_manager import *
+
+
 class WordCountReader(object):
-    pass
+    counter = 0
+
+    def bootstrap(self):
+        sys = SystemManager()
+        sys.create_keyspace('Harvard', SIMPLE_STRATEGY, {'replication_factor': '1'})
+        sys.create_column_family('Harvard', 'WordCount')
+        sys.close()
+
+    def read_data(self):
+        col_family = self._column_family()
+        num_rows = len(list(col_family.get_range()))
+        for count in range(0, num_rows):
+            print col_family.get('row' + str(count), columns=['word', 'count'])
+
+    def load_data(self):
+        f = self._read_file()
+        col_family = self._column_family()
+
+        for line in f:
+            data = line.split('\t')
+            if self._valid_data(data):
+                self._insert_data(col_family, data[0], data[1].strip('\n'))
+
+    def _valid_data(self, result):
+        return result is not None and len(result) is 2
+
+    def _read_file(self):
+        return open('wordcount', 'r')
+
+    def _column_family(self):
+        pool = ConnectionPool('Harvard')
+        return pycassa.ColumnFamily(pool, 'WordCount')
+
+    def _insert_data(self, col_family, text, count):
+        col_family.insert('row' + str(self.counter), {'word': text, 'count': count})
+        self.counter += 1
+
+
+def main():
+    reader = WordCountReader()
+    reader.bootstrap()
+    reader.load_data()
+    reader.read_data()
+
+if __name__ == "__main__":
+    main()
