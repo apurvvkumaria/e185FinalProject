633e8dc6c0e03b5e98f847db48d0843a577dd186
Apurv Kumaria <kumaria.apurv@gmail.com>
2013-05-08 01:35:22 -0500

    Refactoring

--- edu/harvard/e185/word_count_reader.py
+++ edu/harvard/e185/word_count_reader.py
@@ -4,19 +4,20 @@ from pycassa.system_manager import *
 
 
 class WordCountReader(object):
-    counter = 0
+    COUNTER, INPUT_FILE_NAME = (0, 'wordcount')
+    KEY_SPACE, COL_FAMILY, COL1, COL2 = ('Harvard', 'WordCount', 'word', 'count')
 
     def bootstrap(self):
         sys = SystemManager()
-        sys.create_keyspace('Harvard', SIMPLE_STRATEGY, {'replication_factor': '1'})
-        sys.create_column_family('Harvard', 'WordCount')
+        sys.create_keyspace(self.KEY_SPACE, SIMPLE_STRATEGY, {'replication_factor': '1'})
+        sys.create_column_family(self.KEY_SPACE, self.COL_FAMILY)
         sys.close()
 
     def read_data(self):
         col_family = self._column_family()
         num_rows = len(list(col_family.get_range()))
         for count in range(0, num_rows):
-            print col_family.get('row' + str(count), columns=['word', 'count'])
+            print col_family.get('row' + str(count), columns=[self.COL1, self.COL2])
 
     def load_data(self):
         f = self._read_file()
@@ -31,15 +32,15 @@ class WordCountReader(object):
         return result is not None and len(result) is 2
 
     def _read_file(self):
-        return open('wordcount', 'r')
+        return open(self.INPUT_FILE_NAME, 'r')
 
     def _column_family(self):
-        pool = ConnectionPool('Harvard')
-        return pycassa.ColumnFamily(pool, 'WordCount')
+        pool = ConnectionPool(self.KEY_SPACE)
+        return pycassa.ColumnFamily(pool, self.COL_FAMILY)
 
     def _insert_data(self, col_family, text, count):
-        col_family.insert('row' + str(self.counter), {'word': text, 'count': count})
-        self.counter += 1
+        col_family.insert('row' + str(self.COUNTER), {self.COL1: text, self.COL2: count})
+        self.COUNTER += 1
 
 
 def main():
